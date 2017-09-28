da158ee89554515a09b2ef58e224877053b41161
ADamNation1 <32137775+ADamNation1@users.noreply.github.com>
2017-09-28 14:40:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,41 @@
-import sys, os
 
-dirList = []
+#!/usr/bin/python36
+import argparse
+import sys
+import logging
+import os
 
-dirList.append(input("Enter directory you wish to scan: "))
-while True:
-    multiple = input("Another one? (Press enter to contine without.)")
-    if multiple == "":
-        break;
-    else:
-        dirList.append(multiple)
+def list_dir(directory):
+    logging.debug("Listing directory " + directory)
+
+def main():
+    parser = argparse.ArgumentParser(description='py directory scanner')
+    parser.add_argument('-v', '--verbose', help='enable verbose output', action="store_true")
+    parser.add_argument('-d', '--directories', metavar='dir', type=str, nargs='+', required=True, help='one or multiple directories')
+    parser.add_argument('-o', '--output', metavar='dir', type=str, required=True, help='one output directory')
+
+    args = parser.parse_args()
+    output_location = args.output
+
+    if str(output_location).endswith("\\"):
+        output_location.append("\\")
+
+    if(args.verbose):
+        logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+        logging.info("Verbose output enabled")
+
+    for directory in args.directories:
+        logging.info("Creating Output file: " + output_location + "directory1.txt")
+        with open(output_location + "woof.txt", "w") as f:
+            for root, dirs, files in os.walk(directory, topdown=True):
+                for name in files:
+                    print(os.path.join(root, name))
+                    f.write(os.path.join(root, name))
+                for name in dirs:
+                    print(os.path.join(root, name))
+                    f.write((os.path.join(root, name)))
 
 
-print("Directories to scan...")
-for i in dirList:
-    print(i)
 
-proceed = input("Proceed? Y/N: ")
-if proceed.lower() == "n":
-    print("Exiting...")
-    sys.exit()
-print("processing...")
-
-
-
-for i in dirList:
-    with open(str(i) + ".txt", "w") as f:
-        for root, dirs, files in os.walk(i, topdown=True):
-            for name in files:
-                print(os.path.join(root, name))
-                f.write(os.path.join(root, name))
-            for name in dirs:
-                print(os.path.join(root, name))
-                f.write((os.path.join(root, name))
-
+main()
 
