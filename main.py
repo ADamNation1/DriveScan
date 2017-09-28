bd2b1bd3100632139640b1b09f13b1ccc5a405c5
ADamNation1 <32137775+ADamNation1@users.noreply.github.com>
2017-09-28 20:16:57 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ def list_dir(directory):
 
 
 def main():
-    parser = argparse.ArgumentParser(description='py directory scanner')
+    parser = argparse.ArgumentParser(description='Py directory List')
     parser.add_argument('-v', '--verbose', help='enable verbose output', action="store_true")
-    parser.add_argument('-d', '--directories', metavar='dir', type=str, nargs='+', required=True, help='one or multiple directories')
-    parser.add_argument('-o', '--output', metavar='out', type=str, required=True, help='one output directory')
+    parser.add_argument('-d', '--directories', metavar='dir', type=str, nargs='+', required=True, help='One or multiple directories.')
+    parser.add_argument('-o', '--output', metavar='out', type=str, required=True, help='One output directory.')
 
     args = parser.parse_args()
     output_location = args.output
@@ -23,19 +23,25 @@ def main():
         logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
         logging.info("Verbose output enabled")
 
-    if not output_location.endswith('\\'):
-        output_location += "\\"
+    output_location = os.path.abspath(output_location)
+    logging.info("Output Directory: " + output_location)
 
-    for i, directory in enumerate(args.directories):
-        logging.info("Creating Output file: " + output_location + "Directory" + str(i) + ".txt")
-        with open(output_location + "Directory" + str(i) + ".txt", "w") as f:
-            logging.info("Walking...")
-            for dirName, subdirList, fileList in os.walk(directory):
-                logging.info('Directory: %s\n' % dirName)
-                f.write(dirName + "\n")
-                for fileName in fileList:
-                    logging.info('\t%s' % fileName)
-                    f.write("\t" + fileName + "\n")
-                f.write("\n")
+    try:
+        for i, directory in enumerate(args.directories):
+            logging.info("Creating Output file: " + output_location + "Directory" + str(i) + ".txt")
+            with open(output_location + "Directory" + str(i) + ".txt", "w") as f:
+                logging.info("Walking...")
+                for dirName, subdirList, fileList in os.walk(directory):
+                    logging.info('Directory: %s\n' % dirName)
+                    f.write(dirName + "\n")
+                    for fileName in fileList:
+                        logging.info('\t%s' % fileName)
+                        f.write("\t" + fileName + "\n")
+                    f.write("\n")
+        print("Done.")
+    except:
+        print("Something went wrong!")
+
+
 main()
 
